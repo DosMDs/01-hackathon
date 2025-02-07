03ab53c9bed44809c517e04f7b22ddfca72d9526
Oleg Skakunov <dosmds@gmail.com>
2025-02-07 18:40:32 +0300

    Добавил вывод кнопки ОК. Дополнил стили и реализовал логику нажатия

--- src/modal.js
+++ src/modal.js
@@ -4,22 +4,36 @@ class Modal {
       isQuestion: isQuestion
     };
 
-    this.modal = document.querySelector(".modal-overlay");
-    this.modalTitle = document.querySelector(".modal__title");
-    this.modalBody = document.querySelector(".modal__body");
-    this.modalButtons = document.querySelector(".modal__buttons");
+    this.modal = document.querySelector('.modal-overlay');
+    this.modalTitle = document.querySelector('.modal__title');
+    this.modalBody = document.querySelector('.modal__body');
+    this.modalButtons = document.querySelector('.modal__buttons');
 
-    window.addEventListener("click", (event) => {
+    window.addEventListener('click', (event) => {
       if (event.target === this.modal) {
         this.close();
       }
     });
   }
 
-  open(title, content = "") {
+  open(title, content = '') {
     this.modalTitle.innerHTML = title;
     this.modalBody.innerHTML = content;
     this.modal.classList.toggle('modal-overlay_hidden');
+    if (this.state.isQuestion) {
+
+    } else {
+      this._addOkBtn();
+    }
+  }
+
+  _addOkBtn() {
+    const buttonOK = document.createElement('button');
+    buttonOK.classList.add('modal__button', 'modal__confirm-button');
+    buttonOK.textContent = 'OK';
+    this.modalButtons.appendChild(buttonOK);
+
+    buttonOK.addEventListener('click', (event) => this.close());
   }
 
   close() {
