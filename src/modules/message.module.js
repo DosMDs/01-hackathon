4fecf2aa8f8980c01362de8df979dd8671a197d1
Danzan <104309959+BadmaevDanzan@users.noreply.github.com>
2025-02-07 23:10:51 +0300

    message-module переместил функцию setPositionElement и deleteMessageBlock в utils.js

--- src/modules/message.module.js
+++ src/modules/message.module.js
@@ -1,4 +1,5 @@
 import {Module} from '../core/module'
+import {deleteMessageBlock, setPositionElement} from '../utils'
 
 export class MessageModule extends Module {
     constructor() {
@@ -13,27 +14,13 @@ export class MessageModule extends Module {
         const element = this.createDivElement(this.createSpanElement(message), position);
         document.body.append(element);
 
-        this.deleteMessageBlock(element, seconds);
-    }
-
-    deleteMessageBlock(element, seconds) {
-
-
-        setTimeout(() => {
-            element.classList.add('delete-block');
-            if (element.classList.contains('block_rt') || element.classList.contains('block_lt')) element.style.top = '0';
-            if (element.classList.contains('block_rb') || element.classList.contains('block_lb')) element.style.bottom = '0';
-
-            setTimeout(() => {
-                element.remove();
-            }, seconds * 1000);
-        }, 1000);
+        deleteMessageBlock(element, seconds);
     }
 
     createDivElement(spanElement, position) {
         const divElement = document.createElement('div');
         divElement.className = 'message-box';
-        this._setPositionElement(divElement, position);
+        setPositionElement(divElement, position);
 
         divElement.appendChild(spanElement);
         return divElement;
@@ -48,30 +35,4 @@ export class MessageModule extends Module {
 
         return spanElement;
     }
-
-
-    _setPositionElement(element, position) {
-        switch (position) {
-            case 'lt': // left-top
-                element.classList.add('block_lt')
-                break;
-
-            case 'rt': // right-top
-                element.classList.add('block_rt')
-                break;
-
-            case 'rb': // right-bottom
-                element.classList.add('block_rb')
-                break;
-
-            case 'lb': // left-bottom
-                element.classList.add('block_lb')
-                break;
-
-            default:
-                element.classList.add('block_lb')
-                break;
-        }
-    }
-
 }
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,53 @@
 import Modal from './modal';
 
 export function random(min, max) {
-  return Math.round(min - 0.5 + Math.random() * (max - min + 1))
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
 
 export function getRandomColor() {
-  let color = '#' + Math.floor(Math.random() * 16777216).toString(16);
-  return color;
+    let color = '#' + Math.floor(Math.random() * 16777216).toString(16);
+    return color;
 }
 
 export function showModal(title, content = '') {
-  const modal = new Modal();
-  modal.open(title, content);
+    const modal = new Modal();
+    modal.open(title, content);
+}
+
+/* Удаление элемента */
+export function deleteMessageBlock(element, seconds) {
+    setTimeout(() => {
+        element.classList.add('delete-block');
+        if (element.classList.contains('block_rt') || element.classList.contains('block_lt')) element.style.top = '0';
+        if (element.classList.contains('block_rb') || element.classList.contains('block_lb')) element.style.bottom = '0';
+
+        setTimeout(() => {
+            element.remove();
+        }, seconds * 1000);
+    }, 1000);
+}
+
+/* Задаем позицию элементу */
+export function setPositionElement(element, position) {
+    switch (position) {
+        case 'lt': // left-top
+            element.classList.add('block_lt')
+            break;
+
+        case 'rt': // right-top
+            element.classList.add('block_rt')
+            break;
+
+        case 'rb': // right-bottom
+            element.classList.add('block_rb')
+            break;
+
+        case 'lb': // left-bottom
+            element.classList.add('block_lb')
+            break;
+
+        default:
+            element.classList.add('block_lb')
+            break;
+    }
 }
