2afb25cc665125367b886498332f0c60164829dc
Danzan <104309959+BadmaevDanzan@users.noreply.github.com>
2025-02-08 02:34:30 +0300

    random-sounds-module добавил случайный звук

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import ContextMenu from './menu'
 import Modal from './modal';
 import './styles.css'
 import { BackgroundModule } from './modules/background.module'
+import {RandomSoundModule} from "@/modules/sounds.module";
 
 const contextMenu = new ContextMenu();
 // contextMenu.add(new ClicksModule());
 contextMenu.add(new BackgroundModule());
+contextMenu.add(new RandomSoundModule());
