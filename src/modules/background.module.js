0e64112ecb2870f64dce5b6c3fbef90e225a49e7
Firsov <alexsejfirsov@gmail.com>
2025-02-07 19:19:34 +0300

    Реализовал модуль изменения цвета фона в случайном порядке

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,28 +1,12 @@
 import { Module } from '../core/module'
+import { getRandomColor } from '../utils'
 
 export class BackgroundModule extends Module {
   constructor() {
     super("background", "Изменение фона");
   }
 
-  getRandomColor() {
-    let color = '#' + Math.floor(Math.random() * 16777216).toString(16);
-    return color;
-  }
-
   trigger() {
-    Promise.race(
-      Array.from({ length: 5 }, () =>
-        new Promise((resolve) =>
-          setTimeout(() => resolve(this.getRandomColor()), Math.random() * 1500 + 500)
-        )
-      )
-    )
-      .then((color) => {
-        if (document.body) {
-          document.body.style.backgroundColor = color;
-        }
-      })
-      .catch((error) => console.error(error));
+    document.body.style.backgroundColor = getRandomColor();
   }
 } 
