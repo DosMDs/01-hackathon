12a91d72bb978bcdf19e5d031d8520201427c4be
Firsov <alexsejfirsov@gmail.com>
2025-02-07 19:00:24 +0300

    Реализовал изменения цвета

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,28 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
+  constructor() {
+    super("background", "Изменение фона");
+  }
 
-}
+  getRandomColor() {
+    let color = '#' + Math.floor(Math.random() * 16777216).toString(16);
+    return color;
+  }
+
+  trigger() {
+    Promise.race(
+      Array.from({ length: 5 }, () =>
+        new Promise((resolve) =>
+          setTimeout(() => resolve(this.getRandomColor()), Math.random() * 1500 + 500)
+        )
+      )
+    )
+      .then((color) => {
+        if (document.body) {
+          document.body.style.backgroundColor = color;
+        }
+      })
+      .catch((error) => console.error(error));
+  }
+} 
