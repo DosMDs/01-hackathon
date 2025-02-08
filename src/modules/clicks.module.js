9051cb110add3c48b510e250174ff2a48ddfb62f
Oskar <109804658+NioFFi2LLeR@users.noreply.github.com>
2025-02-08 18:17:18 +0300

    добавил обработку двойных кликов

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -6,17 +6,50 @@ export class ClicksModule extends Module {
         super("clicks", "Подсчет кликов за определенное время");
         this.timerContainer = document.createElement('div');
         this.countClick = 0;
-        this.handleClick = () => this.countClick++;
+        this.oneClick = 0;
+        this.doubleClick = 0;
+        this.lastTimeClick = 0;
+        this.handleClick = (event) => {
+            const now = Date.now();
+
+            if(event.detail === 1){
+                if (this.timeout) {
+                    clearTimeout(this.timeout); 
+                }
+
+                this.timeout = setTimeout(() => {
+                    // Если прошло больше времени, считаем одиночным
+                    this.oneClick++;
+                }, 200);
+            }
+
+            if(event.detail === 2) {
+                this.doubleClick++;
+                clearTimeout(this.timeout)
+            }
+
+            this.lastTimeClick = now;
+        };
     }    
     
     trigger() {
+        this.#reset();
+
         setTimeout(() => {
             document.addEventListener('click', this.handleClick);
         }, 1)
 
         createTimer(3, this.timerContainer, () => {
             document.removeEventListener('click', this.handleClick);
-            alert(this.countClick);
+            this.countClick = this.oneClick + this.doubleClick;
+            alert(`Одиночных кликов: ${this.oneClick}, Дабл кликов: ${this.doubleClick}, Всего кликов: ${this.countClick}`);
         });
     };
+
+    #reset() {
+        this.countClick = 0;
+        this.oneClick = 0;
+        this.doubleClick = 0;
+        this.lastTimeClick = 0;
+    }
 }
