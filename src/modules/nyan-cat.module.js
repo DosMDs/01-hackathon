fc79be8ffee7d53993e7960724154c1cf1918ac5
Danzan <104309959+BadmaevDanzan@users.noreply.github.com>
2025-02-08 17:10:29 +0300

    nyancat-module добавлена музыка и фон

--- src/modules/nyan-cat.module.js
+++ src/modules/nyan-cat.module.js
@@ -1,5 +1,6 @@
 import {Module} from '@/core/module'
-import nyancat from '@/resources/img/nyancat.gif'
+import nyancatImg from '@/resources/img/nyancat.gif'
+import backGroundImg from '@/resources/img/background.gif'
 import nyancatSound from '@/resources/sound/nyancat-sound.mp3'
 
 export class NyanCatModule extends Module {
@@ -8,33 +9,47 @@ export class NyanCatModule extends Module {
     }
 
     trigger() {
-        const img = document.createElement('img');
-        img.src = nyancat;
-        img.width = '100';
-        img.height = '75';
-        img.style.position = 'absolute';
-        img.style.top = '50%';
-        img.style.left = '50%';
+        const audio = new Audio(nyancatSound);
+        audio.loop = true;
+        audio.volume = 0.01;
+        audio.play();
+
+
+        const backGround = document.createElement('img');
+        backGround.src = backGroundImg;
+        backGround.style.width = '100%';
+        backGround.style.height = '100%';
+
+        document.body.append(backGround);
+
+
+        const nyanCatImg = document.createElement('img');
+        nyanCatImg.src = nyancatImg;
+        nyanCatImg.width = '150';
+        nyanCatImg.height = '75';
+        nyanCatImg.style.position = 'absolute';
+        nyanCatImg.style.top = '50%';
+        nyanCatImg.style.left = '50%';
 
         let cursorYPosition = null;
         let cursorXPosition = null;
         setInterval(() => {
 
-            let currentLeft = parseInt(img.style.left);
-            let currentTop = parseInt(img.style.top);
+            let currentLeft = parseInt(nyanCatImg.style.left);
+            let currentTop = parseInt(nyanCatImg.style.top);
 
             // Движение влево/вправо
-            if (currentLeft < cursorXPosition - img.width / 2) {
-                img.style.left = (currentLeft + 1) + 'px';
-            } else if (currentLeft > cursorXPosition - img.width / 2) {
-                img.style.left = (currentLeft - 1) + 'px';
+            if (currentLeft < cursorXPosition - nyanCatImg.width / 2) {
+                nyanCatImg.style.left = (currentLeft + 1) + 'px';
+            } else if (currentLeft > cursorXPosition - nyanCatImg.width / 2) {
+                nyanCatImg.style.left = (currentLeft - 1) + 'px';
             }
 
             // Движение вверх/вниз
-            if (currentTop < cursorYPosition - img.height / 2) {
-                img.style.top = (currentTop + 1) + 'px';
-            } else if (currentTop > cursorYPosition - img.height / 2) {
-                img.style.top = (currentTop - 1) + 'px';
+            if (currentTop < cursorYPosition - nyanCatImg.height / 2) {
+                nyanCatImg.style.top = (currentTop + 1) + 'px';
+            } else if (currentTop > cursorYPosition - nyanCatImg.height / 2) {
+                nyanCatImg.style.top = (currentTop - 1) + 'px';
             }
 
         }, 10)
@@ -48,7 +63,7 @@ export class NyanCatModule extends Module {
             cursorYPosition = y;
 
         });
-        document.body.append(img);
+        document.body.append(nyanCatImg);
     }
 
     stopGif(img) {
