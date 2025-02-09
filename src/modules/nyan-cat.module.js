be9147b1dc47587d4bc1f9dff53a3761c031c0b1
Danzan <104309959+BadmaevDanzan@users.noreply.github.com>
2025-02-09 04:39:34 +0300

    message-module добавил повороты и радугу к котику

--- src/modules/nyan-cat.module.js
+++ src/modules/nyan-cat.module.js
@@ -5,7 +5,7 @@ import nyancatSound from '@/resources/sound/nyancat-sound.mp3'
 
 export class NyanCatModule extends Module {
     constructor() {
-        super("nyancat", "Позвать нянкат");
+        super("watch-diarams", "Посмотреть диорамы");
     }
 
     trigger() {
@@ -14,12 +14,11 @@ export class NyanCatModule extends Module {
         audio.volume = 0.01;
         audio.play();
 
-
         const backGround = document.createElement('img');
         backGround.src = backGroundImg;
         backGround.style.width = '100%';
         backGround.style.height = '100%';
-
+        backGround.style.zIndex = '1';
         document.body.append(backGround);
 
 
@@ -27,16 +26,51 @@ export class NyanCatModule extends Module {
         nyanCatImg.src = nyancatImg;
         nyanCatImg.width = '150';
         nyanCatImg.height = '75';
+        nyanCatImg.style.zIndex = '3'
         nyanCatImg.style.position = 'absolute';
-        nyanCatImg.style.top = '50%';
-        nyanCatImg.style.left = '50%';
-
+        nyanCatImg.style.top = (window.innerHeight / 2 - 75 / 2) + 'px';
+        nyanCatImg.style.left = (window.innerWidth / 2 - 150 / 2) + 'px';
         let cursorYPosition = null;
         let cursorXPosition = null;
-        setInterval(() => {
 
+        const menuDiaram = document.createElement('div');
+        menuDiaram.className = 'menu--diaram'
+
+
+        const buttonPrev = document.createElement('button');
+        buttonPrev.className = 'button--prev';
+        buttonPrev.innerText = '<'
+
+        const buttonNext = document.createElement('button');
+        buttonNext.className = 'button--next';
+        buttonNext.innerText = '>'
+
+        const divContainer = document.createElement('div');
+        divContainer.className = 'div--container';
+
+
+        const titleDiagram = document.createElement('span');
+        titleDiagram.innerText = 'Нянкат';
+        titleDiagram.style.margin = 'auto'
+
+        divContainer.append(titleDiagram);
+        menuDiaram.append(buttonPrev, divContainer, buttonNext);
+
+        document.body.append(menuDiaram)
+        let rotation = 0;
+        setInterval(() => {
             let currentLeft = parseInt(nyanCatImg.style.left);
             let currentTop = parseInt(nyanCatImg.style.top);
+
+            let angleRad = Math.atan2(
+                cursorYPosition - nyanCatImg.height / 2 - currentTop,
+                cursorXPosition - nyanCatImg.width / 2 - currentLeft
+            );
+            let angleDeg = angleRad * (180 / Math.PI);
+
+            // Плавно поворачиваем спрайт
+            rotation = rotation + (((angleDeg - rotation + 180) % 360) - 180) * 0.1;
+            nyanCatImg.style.transform = `rotate(${rotation}deg)`;
 
             // Движение влево/вправо
             if (currentLeft < cursorXPosition - nyanCatImg.width / 2) {
@@ -52,16 +86,37 @@ export class NyanCatModule extends Module {
                 nyanCatImg.style.top = (currentTop - 1) + 'px';
             }
 
-        }, 10)
+
+            const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+            const offset = 7;
+
+            setTimeout(() => {
+                colors.forEach((color, index) => {
+                    const div = document.createElement('div');
+                    div.style.position = 'absolute';
+                    div.style.zIndex = '2';
+                    div.style.background = color;
+                    div.style.width = '7px';
+                    div.style.height = '7px';
+                    div.style.borderRadius = '50%';
+
+                    div.style.top = (currentTop + nyanCatImg.height / 2 - 27 + (index * offset)) + 'px';
+                    div.style.left = currentLeft + nyanCatImg.width / 2 + 'px';
+
+                    document.body.append(div);
+
+                    setTimeout(() => {
+                        div.remove();
+                    }, 5000);
+                });
+            }, 15);
+
+        }, 10);
 
 
         document.body.addEventListener('mousemove', (event) => {
-            const x = event.pageX;
-            const y = event.pageY;
-
-            cursorXPosition = x;
-            cursorYPosition = y;
-
+            cursorXPosition = event.pageX;
+            cursorYPosition = event.pageY;
         });
         document.body.append(nyanCatImg);
     }
