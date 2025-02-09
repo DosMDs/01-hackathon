c58dd3e9176d94a943271794ed36e53cd9c6e953
Oleg Skakunov <dosmds@gmail.com>
2025-02-09 10:25:20 +0300

    убрал ненужные комментарии и отступы

--- src/app.js
+++ src/app.js
@@ -8,11 +8,9 @@ import { ClicksModule } from './modules/clicks.module';
 import { MessageModule } from "@/modules/message.module";
 
 const contextMenu = new ContextMenu();
-// contextMenu.add(new ClicksModule());
 contextMenu.add(new BackgroundModule());
 contextMenu.add(new RandomSoundModule());
 contextMenu.add(new ShapeModule());
 contextMenu.add(new TimerModule());
 contextMenu.add(new ClicksModule());
-
 contextMenu.add(new MessageModule());
