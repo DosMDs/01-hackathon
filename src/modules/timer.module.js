5afdb86bb2b686808fc26aa86da5269fdd4d74cb
Oleg Skakunov <dosmds@gmail.com>
2025-02-09 14:14:18 +0300

    выполнил реафакторинг timer.module

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -1,5 +1,5 @@
 import { Module } from "../core/module";
-import { createTimer } from "../utils";
+import { createTimer, showModal, setPositionElement } from "../utils";
 
 class TimerModule extends Module {
   constructor() {
@@ -13,22 +13,14 @@ class TimerModule extends Module {
     );
 
     if (isNaN(seconds) || seconds <= 0) {
-      alert("Введите корректное число секунд!");
+      showModal("Введите корректное число секунд!");
       return;
     }
 
     const timerContainer = document.createElement("div");
-    timerContainer.style.position = "fixed";
-    timerContainer.style.top = "20px";
-    timerContainer.style.right = "20px";
-    timerContainer.style.background = "black";
-    timerContainer.style.color = "white";
-    timerContainer.style.padding = "10px 15px";
-    timerContainer.style.borderRadius = "5px";
-    timerContainer.style.fontSize = "18px";
-    timerContainer.style.fontWeight = "bold";
-
-    createTimer(seconds, timerContainer, () => alert("Время вышло!"));
+    timerContainer.className = "timer";
+    setPositionElement(timerContainer, "rt");
+    createTimer(seconds, timerContainer, () => showModal("Время вышло!"));
   }
 }
 
